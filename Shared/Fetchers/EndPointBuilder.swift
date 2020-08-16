c6783827aa486f9114f27d7432b406f64c1c1a1c
Bill Calkins <bill@calkinsconsulting.com>
2020-08-15 19:24:19 -0500

    got fetcher code compiling.

---
+++ Shared/Fetchers/EndPointBuilder.swift
@@ -0,0 +1,25 @@
+//
+//  EndPointBuilder.swift
+//  CovidNumbers
+//
+//  Created by Bill Calkins on 8/15/20.
+//
+
+import Foundation
+
+enum StateSorts: String {
+    case cases, todayCases, deaths, active
+}
+
+struct DISEASESH_API {
+    
+    static let schema = "https"
+    static let host = "disease.sh"
+    static let version = "v3"
+    static let basePath = "\(version)/covid-19"
+    
+    static let statesBasePath = "\(basePath)/states"
+    static let allBasePath = "\(basePath)/all"
+    static let countriesPath = "\(basePath)/countries"
+    
+}
--- Shared/Fetchers/StatesFetcher.swift
+++ Shared/Fetchers/StatesFetcher.swift
@@ -21,11 +21,11 @@ class StatesFetcher {
 //MARK:- Fetchable
 extension StatesFetcher: StatesFetchable {
     func fetchAllStates() -> StatesPublisher {
-        
+        return stateItems(with: self.makeAllStateComponents(sortBy: .active, includeYesterday: true))
     }
     
     func fetchGivenStates(stateToSearch givenStates: [String]) -> StatesPublisher {
-        
+        return stateItems(with: self.makeSpecificStateComponents(givenStates: givenStates))
     }
     
     //MARK:- Network Adaptor
@@ -44,8 +44,33 @@ extension StatesFetcher: StatesFetchable {
                 decode(returnedPair.data)
             }
             .eraseToAnyPublisher()
+    }
+}
+
+//MARK:- Endpoint Builder
+private extension StatesFetcher {
+    
+    func makeAllStateComponents(sortBy sort: StateSorts, includeYesterday yesterday: Bool) -> URLComponents {
         
+        var components = URLComponents()
+        
+        components.scheme = DISEASESH_API.schema
+        components.host = DISEASESH_API.host
+        components.path = DISEASESH_API.statesBasePath
+        components.queryItems = [URLQueryItem(name: "sort", value: sort.rawValue), URLQueryItem(name: "yesterday", value: yesterday ? "true" : "false")]
+        
+        return components
     }
     
+    func makeSpecificStateComponents(givenStates states: [String]) -> URLComponents {
+        
+        var components = URLComponents()
+        
+        components.scheme = DISEASESH_API.schema
+        components.host = DISEASESH_API.host
+        components.path = DISEASESH_API.statesBasePath + "/\(states.joined(separator: ","))"
+        
+        return components
+    }
 }
 
